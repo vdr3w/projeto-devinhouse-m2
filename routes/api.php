3f9670df3b334002a0f988761f31ad7c7d87cc77
Drew <84882983+vdr3w@users.noreply.github.com>
2023-12-18 16:53:24 -0300

    feat(LOGIN/LOGOUT): Autenticação de usuário e gestão de tokens

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,21 @@
 
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 
 Route::middleware('auth:sanctum')->group(function () {
     // rotas privadas
+
+    //LOGIN
+    Route::post('logout', [AuthController::class, 'logout']);
 });
 
 // Rotas públicas que somente depois ficaram privadas
 
 //USER
 Route::post('users', [UserController::class, 'store']);
+
+
+//LOGIN
+Route::post('login', [AuthController::class, 'login']);
