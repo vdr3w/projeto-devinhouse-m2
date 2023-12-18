00180db67b3288c62e60367928cf4b660b7c3c74
Drew <84882983+vdr3w@users.noreply.github.com>
2023-12-18 18:37:16 -0300

    feat(DASHBOARD): Utilizado descrição e limite do plano para cálculo de estudantes restantes

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,24 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
 
 Route::middleware('auth:sanctum')->group(function () {
-    // rotas privadas
+// rotas privadas
 
-    //LOGIN
-    Route::post('logout', [AuthController::class, 'logout']);
+//LOGOUT
+Route::post('logout', [AuthController::class, 'logout']);
+
+//DASHBOARD
+Route::get('dashboard', [DashboardController::class, 'index']);
 });
 
 // Rotas públicas que somente depois ficaram privadas
 
 //USER
 Route::post('users', [UserController::class, 'store']);
-
 
 //LOGIN
 Route::post('login', [AuthController::class, 'login']);
