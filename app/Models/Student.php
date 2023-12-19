e3e955d7d2af6d8661e5985e5d972eba02caf709
Drew <84882983+vdr3w@users.noreply.github.com>
2023-12-19 19:54:03 -0300

    feat(STUDENTS): Implementada criação, deleção (soft delete), listagem (com agrupamento de endereço) e atualização de estudantes

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,12 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Student extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    protected $table = 'students_test';
+        protected $fillable = [
+        'name',
+        'email',
+        'date_birth',
+        'cpf',
+        'contact',
+        'user_id',
+        'city',
+        'neighborhood',
+        'number',
+        'street',
+        'state',
+        'cep'
+    ];
+
+        protected $hidden = ['created_at', 'updated_at'];
+
+        public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
